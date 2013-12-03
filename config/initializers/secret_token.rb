bdfc26349bf5a00517b9db9c3ca98dd4f5618bae
Randy Lee <randyrlee@gmail.com>
2013-12-03 09:22:05 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Halfgap::Application.config.secret_key_base = '53fd563207ad0166fad49e3fd09b8846830cfbf2876b337a2494cbffdbf7456c4463f752b60e9a7e8e300a84de73ed2297565b4b95c55ce0485a3a1dde3b482a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Halfgap::Application.config.session_store :cookie_store, key: '_halfgap_session'
