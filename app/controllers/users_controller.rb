a8d814988197d36bf21e0e280c7bff72f8fdff7a
Randy Lee <randyrlee@gmail.com>
2013-12-04 18:33:04 -0800

    added instant messaging to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,6 @@ class UsersController < ApplicationController
 		if @user.save
 			redirect_to @user
 		end
-		user = User.find_by(email: params[:user][:email])
-		if user.authenticate(params[:user][:password])
-        session[:user_id] = user.id
-        redirect_to user
-    end
 	end
 
 	def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Halfgap::Application.routes.draw do
 
   resources :users
+
+
+  namespace :api, defaults: {format: :json} do
+    resources :task_lists, only: [:index] do
+      resources :tasks, only: [:index, :create, :update, :destroy]
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
