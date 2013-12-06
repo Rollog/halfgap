a6b357e836ac7743c7905b2e322af08d7d79820e
Randy Lee <randyrlee@gmail.com>
2013-12-06 11:09:33 -0800

    removed websocket shit successfully

--- spec/features/user_sends_instant_message_spec.rb
+++ spec/features/user_sends_instant_message_spec.rb
@@ -1,34 +1,34 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-# In order to chat with friends,
-# as a user
-# I want to send a message
+# # In order to chat with friends,
+# # as a user
+# # I want to send a message
 
-# Scenario 1: Success
-# When I login to my user profile
-# and I message a friend I want to talk to
-# and I hit enter
-# Then the page should display the
-# message sent
+# # Scenario 1: Success
+# # When I login to my user profile
+# # and I message a friend I want to talk to
+# # and I hit enter
+# # Then the page should display the
+# # message sent
 
-feature "user sends an instant message", js: true do
-  before :each do
-    User.create(name: 'user@example.com', password: 'caplin')
-  end
+# feature "user sends an instant message", js: true do
+#   before :each do
+#     User.create(name: 'user@example.com', password: 'caplin')
+#   end
 
-  context "successfully" do
-    scenario "sending a instant message" do
-      visit(user_path(User.first))  #user_path
+#   context "successfully" do
+#     scenario "sending a instant message" do
+#       visit(user_path(User.first))  #user_path
 
-      fill_in 'user_name', with: 'Marc'
-      fill_in 'message', with: 'hi there!'
+#       fill_in 'user_name', with: 'Marc'
+#       fill_in 'message', with: 'hi there!'
 
-      click_button 'send'
+#       click_button 'send'
 
-      # expect(page).to have_content 'Marc'
-      expect(page).to have_content 'hi there!'
+#       # expect(page).to have_content 'Marc'
+#       expect(page).to have_content 'hi there!'
 
-    end
-  end
+#     end
+#   end
 
-end
+# end
