38f382bf66e4d115277f3b0eb8a24b14deab09d6
Randy Lee <randyrlee@gmail.com>
2013-12-07 18:09:08 -0800

    added login through userbin and user can delete friendships

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,16 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
   
   def current_user
-  	if session[:user_id]
-	    @current_user ||= User.find_by(id: session[:user_id])
-  	end
-  end
-
-  def share_variables
-    Thread.current[:current_user] = current_user
-    # Thread.current[:current_ip] = request.remote_ip
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
   def authenticate_user
-    redirect_to authentications_url unless @current_user
+    redirect_to authentications_url unless current_user
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
 end
