60646e9efa8519ecdad30ce42cbd9b357df666e3
marcwright <marc.wright@gmail.com>
2013-12-16 12:08:11 -0800

    committing before my new pull

--- app/models/meet_request.rb
+++ app/models/meet_request.rb
@@ -1,6 +1,6 @@
 class MeetRequest < ActiveRecord::Base
 
-	belongs_to :sender, class_name: "User", foreign_key: "sender_id", primary_key: "sender_id"
+	belongs_to :sender, class_name: "User", foreign_key: "sender_id"
 	belongs_to :receiver, class_name: "User", foreign_key: "receiver_id", primary_key: "sender_id"
 
 end
