53b705686b9e867e985d80c9219994e77e8be897
Randy Lee <randyrlee@gmail.com>
2013-12-15 21:27:54 -0800

    i keep forgeting to switch to the fucking master branch FUCK

--- app/controllers/meet_requests_controller.rb
+++ app/controllers/meet_requests_controller.rb
@@ -4,28 +4,16 @@ class MeetRequestsController < ApplicationController
 		@meet_requests = current_user.received_requests
 	end
 
-	# def new
-	# @friendship = current_user.friendships.friend.find(params[:id])
-	# @meet_request = @friendship.meet_requests.new
-	#  if params[:reply_to]
-	# 	 @reply_to = User.find_by_user_id(params[:reply_to])
-	# 	 unless @reply_to.nil?
-	# 		 @meet_request.receiver_id = @reply_to.user_id
-	# 	 end
-	# 	end
- #  end
-
  	def new
  		@meet_request = current_user.sent_requests.new
- 		@friend = current_user.friends.find_by(params[:id])
  	end
 
 	def create
 		@friend = current_user.friends.find_by(params[:id])
-
 		@meet_request = current_user.sent_requests.new(meet_requests_params)
 		# to make sure we are sending storing to whom the req is being sent
 		@meet_request.receiver_id = @friend.id
+
 		if @meet_request.save
 			flash[:notice] = "Request Sent!"
 			redirect_to current_user
@@ -37,8 +25,17 @@ class MeetRequestsController < ApplicationController
 
 	def confirm_meetup
 		@received_request = current_user.received_requests.find_by(params[:receiver_id])
-		@received_request.update_attributes(meetup_confirmation: true)
-		redirect_to :back
+		if @received_request.update_attributes(meetup_confirmation: true)
+			redirect_to @received_request
+		end
+	end
+
+	def show
+		@friend = current_user.friends.find_by(params[:id])
+		# once a user confirms a request user is taken to map route
+		@received_request = current_user.received_requests.find_by(params[:receiver_id])
+		@received_request.sender_id = @friend.id
+		flash[:error] = "Meet request not confirmed!"
 	end
 
 	private
